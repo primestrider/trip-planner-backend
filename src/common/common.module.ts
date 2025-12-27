2e6fa66a38833f491577c23b1fcc97f53e511e86
Ricky <thechaosmyth7@gmail.com>
2025-12-27 22:14:23 +0700

    feat: add prisma service

---
+++ src/common/common.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from "@nestjs/common";
+import { WinstonModule } from "nest-winston";
+import winston from "winston";
+import { ConfigModule } from "@nestjs/config";
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      level: "debug",
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ]
+})
+export class CommonModule {}
